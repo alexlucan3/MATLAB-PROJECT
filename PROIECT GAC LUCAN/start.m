853ee0ec5f7d904cb9b33aa9cab38bac63a9fba6
alexlucan3 <59365967+alexlucan3@users.noreply.github.com>
2022-06-02 12:14:36 +0300

    Add files via upload

---
+++ PROIECT GAC LUCAN/start.m
@@ -0,0 +1,11 @@
+close all;
+clear;
+      
+       N=6;
+       A=100; %amplitudine sinus
+       R=12; %valoarea rezistentei
+       L=0.15; %valoarea inductantei
+       f=40; %frecventa
+       lista =0;
+       
+interfata(A,R,L,f,lista,N);
